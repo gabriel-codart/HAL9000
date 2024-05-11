932742561a31ec28e0dc734ea29f826a2a49195c
gabriel-codart <gabriel.oliveira.codart@gmail.com>
2024-05-10 23:41:25 -0300

    Estruturação do Desing

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,8 +3,34 @@ import 'react';
 function Home() {
   return (
     <>
+      <div id='hal-eye'>
+        <img
+          src="/hal-9000.png"
+          alt="hal-9000"
+          width={"150rem"}
+        />
+      </div>
+
+      <br />
+      
       <h1>Olá, eu sou o HAL9000</h1>
       <p>Em que posso ajudá-lo?</p>
+
+      <br />
+
+      <div className='input-group'>
+        <input
+          type="text"
+          placeholder='Digite algo...'
+        />
+        <button
+          className='btn-send'
+          onClick={() => {}}
+        >
+          ➜
+        </button>
+      </div>
+      
     </>
   )
 }
